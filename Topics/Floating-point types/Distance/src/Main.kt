6b20e1233ccae68a6c5d94bead505e9950ae123e
paulrwade <pau2237029@maricopa.edu>
2025-04-03 17:49:34 -0700

    Iterative check in

---
+++ Topics/Floating-point types/Distance/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    val distance =  readln().toInt()
+    val time = readln().toInt()
+
+    println(distance / time.toDouble())
+
+}
--- Topics/Hierarchy of exceptions/IndexOutOfBoundsException/src/Main.kt
+++ Topics/Hierarchy of exceptions/IndexOutOfBoundsException/src/Main.kt
@@ -1,2 +1,21 @@
 val list = listOf(1, 2, 3)
 println(list[3])
+
+
+
+
+
+
+
+/*
+fun main() {
+
+    val list = listOf(1, 2, 3)
+
+    try {
+        println(list[3]) // This will throw an IndexOutOfBoundsException
+    } catch (e: IndexOutOfBoundsException) {
+        //println("Caught an IndexOutOfBoundsException: ${e.message}")
+    }
+}
+*/
